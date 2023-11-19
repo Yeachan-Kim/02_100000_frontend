b317b6cf8f6682fc77badf163cf07ed20b521ce5
Jonghyeok <jhpmuscle@gmail.com>
2023-11-20 02:24:36 +0900

    feat: initial setting, page/main layout

---
+++ public/js/footer.js
@@ -0,0 +1,7 @@
+const footer = $("footer");
+
+$("document").ready(function () {
+    footer.load("../../pages/include/footer.html", function () {
+
+    })
+})
---
+++ public/js/navbar.js
@@ -0,0 +1,7 @@
+const navbar = $("nav");
+
+$("document").ready(function () {
+    navbar.load("../../pages/include/navbar.html", function () {
+
+    })
+})
