0a023366c458eb1ae3075b83f67c437a483ba674
Jonghyeok <jhpmuscle@gmail.com>
2023-11-25 06:00:26 +0900

    merge

--- public/js/footer.js
+++
@@ -1,7 +0,0 @@
-const footer = $("footer");
-
-$("document").ready(function () {
-    footer.load("../../pages/include/footer.html", function () {
-
-    })
-})
--- public/js/mapInit.js
+++
@@ -1,9 +0,0 @@
-let map;
-function myMap() {
-    let mapOptions = {
-        center: new google.maps.LatLng(34.33, 130.1234),
-        zoom: 16
-    };
-
-    map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
-}
